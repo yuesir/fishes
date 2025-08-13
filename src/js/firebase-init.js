f60d27cbe9e3ff1ee49b8fd9b80615df03a4b1af
qinjunkun <qinjunkun@yeah.net>
2025-08-13 15:50:02 +0800

    firebase optimize

--- src/js/firebase-init.js
+++ src/js/firebase-init.js
@@ -1,12 +1,22 @@
 // Firebase config is not private so I don't mind having it in a public repository
+// const firebaseConfig = {
+//   apiKey: "AIzaSyB0SvfVvI8Eq1ZUhzCgJHRqJKKhanWABTI",
+//   authDomain: "checkboxgame.firebaseapp.com",
+//   projectId: "checkboxgame",
+//   storageBucket: "checkboxgame.firebasestorage.app",
+//   messagingSenderId: "571679687712",
+//   appId: "1:571679687712:web:68f1a057e0592d1739e845"
+// };
+
 const firebaseConfig = {
-  apiKey: "AIzaSyB0SvfVvI8Eq1ZUhzCgJHRqJKKhanWABTI",
-  authDomain: "checkboxgame.firebaseapp.com",
-  projectId: "checkboxgame",
-  storageBucket: "checkboxgame.firebasestorage.app",
-  messagingSenderId: "571679687712",
-  appId: "1:571679687712:web:68f1a057e0592d1739e845"
-};
+    apiKey: "AIzaSyB0-o6epZkGFoFJxoRIORCgzV2UuZ0YNcE",
+    authDomain: "drawafish-61cc9.firebaseapp.com",
+    projectId: "drawafish-61cc9",
+    storageBucket: "drawafish-61cc9.firebasestorage.app",
+    messagingSenderId: "5595665832",
+    appId: "1:5595665832:web:53fa16f453f145e7e3399d",
+    measurementId: "G-QRX07J1D8X"
+  };
 
 firebase.initializeApp(firebaseConfig);
 window.db = firebase.firestore();
